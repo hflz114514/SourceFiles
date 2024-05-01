198fef375cf86e615a5264109fb262389e305285
hflz114514 <hflz114514@outlook.com>
2024-05-01 11:50:27 +0800

    modified:   1.cpp

--- 1.cpp
+++ 1.cpp
@@ -8,7 +8,7 @@ int main()
     fopen_s(&ips, "ips.txt", "r");
     for (unsigned ip[4], mask; fscanf_s(ips, "%hu.%hu.%hu.%hu/%hu", ip, ip + 1, ip + 2, ip + 3, &mask);)
     {
-        for (unsigned long long i = ip[0] << 24 | ip[1] << 16 | ip[2] << 8 | ip[3]; i ^ (2 << mask); i++)
+        for (unsigned long long i = ip[0] << 24 | ip[1] << 16 | ip[2] << 8 | ip[3]; i ^ (i >> mask << mask) ^ (2 << mask - 1); i++)
             ;
     }
 }
