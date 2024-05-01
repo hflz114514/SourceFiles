bd7c3fb03ce948c5ad4e6a69add85ebc867734d2
hflz114514 <hflz114514@outlook.com>
2024-05-01 11:41:51 +0800

    new file:   1.cpp

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <bitset>
+#include <cstdio>
+#include <cstring>
+int main()
+{
+    std::bitset<4294967296> vip;
+    FILE *ips;
+    fopen_s(&ips, "ips.txt", "r");
+    for (unsigned ip[4], mask; fscanf_s(ips, "%hu.%hu.%hu.%hu/%hu", ip, ip + 1, ip + 2, ip + 3, &mask);)
+    {
+        for (unsigned long long i = ip[0] << 24 | ip[1] << 16 | ip[2] << 8 | ip[3]; i ^ (2 << mask); i++)
+            ;
+    }
+}
